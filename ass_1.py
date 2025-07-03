1a793db48c94e5d6afdbcb9532e7d9bb021c7a27
ukaveri <168158849+ukaveri@users.noreply.github.com>
2025-07-03 10:33:32 +0530

    Add files via upload

--- ass_1.py
+++ ass_1.py
@@ -12,12 +12,18 @@ for i in range(1,n+1):
     print(st*i)
 """
 #print the sum of the list:
-l = []
-l = input("Enter the numbers")
+"""
+l = list(map(int,input("Enter the numbers").split(' ')))
 s = 0
 for i in l:
-     s = s+int(i)
-
-print(s)
-     
-        
+    s = s+i
+print("sum",s)
+"""
+#reversing a number in a list
+"""
+l = list(map(int,input("Enter the numbers").split(' ')))
+n = len(l)
+r = []
+r = l[::-1]
+print(r)
+"""
