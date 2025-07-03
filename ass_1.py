0adab359af376698d785784f75970417299c6cd0
ukaveri <168158849+ukaveri@users.noreply.github.com>
2025-07-03 09:45:39 +0530

    Add files via upload

---
+++ ass_1.py
@@ -0,0 +1,23 @@
+# pattren 2
+"""
+*
+* *
+* * *
+
+
+st = "*"
+inc= 0
+n = int(input("Enter the rows"))
+for i in range(1,n+1):
+    print(st*i)
+"""
+#print the sum of the list:
+l = []
+l = input("Enter the numbers")
+s = 0
+for i in l:
+     s = s+int(i)
+
+print(s)
+     
+        
